5eec9808f9bc17fa17507a9ce4a0bd7dd5d1e8b5
bobtndm <30938318+bobtndm@users.noreply.github.com>
2017-11-05 12:35:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ And Modified by Farzain - zFz ( Faraaz )
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'ACCESS TOKEN ENTE'; //Your Channel Access Token
-$channelSecret = 'SECRET TOKEN ENTE';//Your Channel Secret
+$channelAccessToken = 'dmik3CLnJ7PzV5l+q+96ZiON/dWSN88ybAzzorrNpz+Xrots/IrAJkm8f9CqaVVP9+iYV39tCIhBUhiY1zzMUjf0KdizAfJr6f6/waGMZaLm+CR8abdlpFczZNTPI+mmLkt2uve1/Y6TmyXMIQEMbAdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = '65737d087511e4ca7e413fe56e5d91c1';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -35,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'API KEY SIMSIMI ENTE'; //API SimSimi
+$key = 'bf9d1a92-b97d-4aea-a863-2b3f2d3b9ea5'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
